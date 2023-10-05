225f3e42dc164c315822347c63eff1e8f8e0280a
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2023-10-05 11:30:24 +0200

    1.1.1

---
+++ src/Presets/CiusAtGov.php
@@ -0,0 +1,16 @@
+<?php
+namespace PhenyxInvoicing\Presets;
+
+/**
+ * CIUS-AT-GOV
+ * @author Bundesrechenzentrum (BRZ)
+ * @link https://www.erechnung.gv.at/files/en16931/CIUS-AT-GOV-XS-1.1.0.pdf
+ */
+class CiusAtGov extends CiusAtNat {
+    /**
+     * @inheritdoc
+     */
+    public function getSpecification(): string {
+        return parent::getSpecification() . "#compliant#urn:erechnung.gv.at:CIUS-AT-GOV:1.1.0";
+    }
+}
